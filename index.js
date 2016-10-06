ee423d3268239ed9fd3b1c8efcf4f783b1aa9170
Telahnus <quan.jordan@gmail.com>
2016-10-06 16:08:09 -0400

    redid geocoding
    C+P'd the example code that makes use of the geocoder api object

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// * * * * * * * * * * * * * * * * * * * * 
-// global variables
+/* 
+	index.js
+	should contain only stuff NOT related to the map
+	aka: buttons, displays, forms
+*/
+
+// GLOBAL VARS
 
 var start_coords = false;
 var dest_coords = false;
 var distancePerDay;
-//keys should be in a seperate file and just included, do not upload to github
-var gmapkey = "AIzaSyAwe6XMoayWw_yOH48YW4zBNf2XGnyZYeo";
 
+// DOC READY AND BUTTON CALLS
 
-// * * * * * * * * * * * * * * * * * * * * 
-// all the button function call
 $(document).ready(function(){
 	$("#dest_known").click(function(){
 		if ($(this).is(':checked')){
@@ -24,49 +26,14 @@ $(document).ready(function(){
 	$("#start_search").click(start_search);
 });
 
-// * * * * * * * * * * * * * * * * * * * * 
-// the actual functions go here
-// keep in mind the async callbacks!!
+
+// FUNCTIONS
+
 function showVal(km){
 	$("#distance").text(km);
 }
 
 function start_search(){
-	//first, check inputs
-	start_coords = get_coord($("#dep_city").val());
-	console.log(start_coords);
-	if (start_coords===false){return false;}
-	if ($("#dest_known").is(':checked')){
-		dest_coords = get_coord($("#dest_city").val());
-		if (dest_coords===false){return false;}
-	}
-	distancePerDay = $("#distance_per_day").val();
-	console.log(start_coords+","+dest_coords+","+distancePerDay);
-}
-
-
-//getting fucked by async callbacks
-// i want to return true or false
-// but calling functions dont wait for return
-function get_coord(cityName){
-	city = encodeURIComponent(cityName);
-	var url = "https://maps.googleapis.com/maps/api/geocode/json?address="+city+"&key="+gmapkey;
-	$.getJSON({
-		url: url,
-		async: false,
-		success: function(result){
-			if (result.status!="OK"){
-				console.log("error returning coordinates: "+result.status);
-				return false;
-			} else {
-				var lat = result.results[0].geometry.location.lat;
-				var long = result.results[0].geometry.location.lng;
-				return [lat,long];
-			}
-		},
-		error: function (xhr, ajaxOptions, thrownError) {
-			console.log("error getting coordinates: "+xhr.status+"; "+thrownError);
-			return false;
-		}
-	});
+	dep_city_string = $("#dep_city").val();
+	geocodeAddress(dep_city_string);
 }
--- map.js
+++ map.js
@@ -1,59 +1,57 @@
+/*
+	map.js
+	This file shoudl contain all variable and functions related to the use of google maps api
+*/
+
+// GLOBAL VARS
+
 var start;
 var end;
 var origin;
-var dest ;
+var dest;
 var bypass=[];
+
+// MAP OBJECTS
+
 var map;
+var geocoder;
+var directionsService;
+var directionsDisplay;
+
+// KEY - should be kept seperate
+
+var gmapkey = "AIzaSyAwe6XMoayWw_yOH48YW4zBNf2XGnyZYeo";
+
+// FUNCTIONS
 
 function initMap() {	
-	var directionsService = new google.maps.DirectionsService;
-	var directionsDisplay = new google.maps.DirectionsRenderer;
+	
 	map = new google.maps.Map(document.getElementById('map'), {
 		mapTypeControl: false,
 		streetViewControl: false,
 		zoom: 6,
 		center: {lat: 41.85, lng: -87.65}
 	});
+
+	geocoder = new google.maps.Geocoder();
+
+	directionsService = new google.maps.DirectionsService;
+
+	directionsDisplay = new google.maps.DirectionsRenderer;
 	directionsDisplay.setMap(map);
-	/*$("#start_search").click(function(){
-		//debugger;
-		start = coord($("#dep_city").val());
-		end = coord($("#dest_city").val());
-		origin = new google.maps.LatLng(start[0],start[1]);
-		dest = new google.maps.LatLng(end[0],end[1]);
-		calculateAndDisplayRoute(directionsService,directionsDisplay);
-	});*/
-	$(".add_city").click(function(){
-		calculateAndDisplayRoute(directionsService,directionsDisplay);
-	});
+
 }
 
-function calculateAndDisplayRoute(directionsService,directionsDisplay) {		
-	var city_arr = $(".city[data-set='true']");
-	var wypts;
-	for(var i=1;i<city_arr.length-1;i++){
-		var city = city_arr[i].getAttribute("data-name");
-		var coords = coord(city);
-		var loc = new google.maps.LatLng(coords[0],coords[1]);
-		wypts= {location: loc, stopover: true};
-		bypass[i-1]=wypts;
-	}
-	directionsService.route({
-		//origin and dest needs to add province to value, otherwise map doesn't work
-		origin: origin,
-		destination: dest,
-		waypoints: bypass,
-		optimizeWaypoints: true,
-		travelMode: google.maps.TravelMode.DRIVING
-	}, 
-	function(response, status) {
-		if (status == google.maps.DirectionsStatus.OK) {
-			directionsDisplay.setDirections(response);
-			var route = response.routes[0];
-			var summaryPanel = document.getElementById("directions_panel");
-			summaryPanel.innerHTML = "";
+function geocodeAddress(address) {
+	geocoder.geocode({'address': address}, function(results, status) {
+		if (status === 'OK') {
+			map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+				map: map,
+				position: results[0].geometry.location
+			});
 		} else {
-			alert("directions response " + status);
+			alert('Geocode was not successful for the following reason: ' + status);
 		}
 	});
 }
