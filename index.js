a20b5cac0bf7cf1d6ba7ddd0823d86bee40e5baa
Telahnus <quan.jordan@gmail.com>
2016-10-07 16:39:32 -0400

    route display
    have basic route display working, removed superflous files, next step is
    to query for cities

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ $(document).ready(function(){
 	$("#start_search").click(start_search);
 });
 
-
 // FUNCTIONS
 
 // dynamic display of search range when using slider
@@ -24,37 +23,22 @@ $(document).ready(function(){
 function showVal(km){
 	$("#distance").text(km);
 }
-// returns true if a string is empty, null or undefined
-function isEmpty(str) {
-    return (!str || 0 === str.length);
-}
 
 function start_search(){
 	dep_string = $("#dep_city").val();
 	dest_string = $("#dest_city").val();
 	check_dep();
 }
-function check_dep(){
-	geocoder.geocode({'address': dep_string}, function(results, status) {
-		if (status === 'OK') {
-			dep_coords = results[0].geometry.location;
-			check_dest();
-		} else {
-			console.log('Geocode was not successful for the following reason: ' + status);
-		}
-	});
-}
-function check_dest(){
-	geocoder.geocode({'address': dest_string}, function(results, status) {
-		if (status === 'OK') {
-			dest_coords = results[0].geometry.location;
-			init_route();
-		} else {
-			console.log('Geocode was not successful for the following reason: ' + status);
-		}
-	});
+
+function next_step(){
+	console.log("reached next step!");
+	$("#startpanel").slideUp();
 }
 
-function init_route(){
-	console.log("next step is to initialize the route!");
+
+// HELPER FUNCTIONS
+
+// returns true if a string is empty, null or undefined
+function isEmpty(str) {
+	return (!str || 0 === str.length);
 }
--- map.js
+++ map.js
@@ -9,7 +9,8 @@ var dep_coords;
 var dest_coords;
 var origin;
 var dest;
-var bypass=[];
+var waypoints=[];
+var route;
 
 // MAP OBJECTS
 
@@ -26,25 +27,57 @@ var gmapkey = "AIzaSyAwe6XMoayWw_yOH48YW4zBNf2XGnyZYeo";
 
 // initialize map and services
 function initMap() {	
-	
 	map = new google.maps.Map(document.getElementById('map'), {
 		mapTypeControl: false,
 		streetViewControl: false,
 		zoom: 6,
 		center: {lat: 41.85, lng: -87.65}
 	});
-
 	geocoder = new google.maps.Geocoder();
-
 	directionsService = new google.maps.DirectionsService;
-
 	directionsDisplay = new google.maps.DirectionsRenderer;
 	directionsDisplay.setMap(map);
-
+}
+function check_dep(){
+	geocoder.geocode({'address': dep_string}, function(results, status) {
+		if (status === 'OK') {
+			dep_coords = results[0].geometry.location;
+			check_dest();
+		} else {
+			console.log('Geocode was not successful for the following reason: ' + status);
+		}
+	});
+}
+function check_dest(){
+	geocoder.geocode({'address': dest_string}, function(results, status) {
+		if (status === 'OK') {
+			dest_coords = results[0].geometry.location;
+			calculateAndDisplayRoute();
+		} else {
+			console.log('Geocode was not successful for the following reason: ' + status);
+		}
+	});
+}
+function calculateAndDisplayRoute() {
+	var request = {
+		origin: dep_coords,
+		destination: dest_coords,
+		travelMode: 'DRIVING',
+		waypoints: waypoints
+	};
+	directionsService.route(request, function(response, status) {
+		if (status === 'OK') {
+			directionsDisplay.setDirections(response);
+			route = response.routes[0];
+			next_step();
+		} else {
+			console.log('Directions request failed due to ' + status);
+		}
+	});
 }
 
 // take string input, return lat/long coordinates
-function geocodeAddress(address, output, callback) {
+/*function geocodeAddress(address, output, callback) {
 	geocoder.geocode({'address': address}, function(results, status) {
 		if (status === 'OK') {
 			output = results[0].geometry.location;
@@ -54,8 +87,7 @@ function geocodeAddress(address, output, callback) {
 			console.log('Geocode was not successful for the following reason: ' + status);
 		}
 	});
-}
-
+}*/
 /*function centreMap(address, callback){
 	geocoder.geocode({'address': address}, function(results, status) {
 		if (status === 'OK') {
